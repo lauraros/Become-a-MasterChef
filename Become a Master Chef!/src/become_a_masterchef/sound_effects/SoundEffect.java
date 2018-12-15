baec5629d668a0e3151b3ed275eca8505aa4f4e7
lauraros <lauraros@users.noreply.github.com>
2018-12-15 10:56:21 +0100

    soundEffect removed method

--- Become a Master Chef!/src/become_a_masterchef/sound_effects/SoundEffect.java
+++ Become a Master Chef!/src/become_a_masterchef/sound_effects/SoundEffect.java
@@ -24,14 +24,6 @@ public enum SoundEffect {
 	WRONG("sounds/Down.wav"),
 	MOVE("sounds/click.wav");
 
-	// Nested class for specifying volume
-	public static enum Volume {
-		MUTE, LOW, MEDIUM, HIGH
-	}
-
-
-	public static Volume volume = Volume.LOW;
-
 	// Each sound effect has its own clip, loaded with its own sound file.
 	private Clip clip;
 
@@ -61,12 +53,11 @@ public enum SoundEffect {
 
 	// Play or Re-play the sound effect from the beginning, by rewinding.
 	public void play() {
-		if (volume != Volume.MUTE) {
-			if (clip.isRunning())
-				clip.stop();   // Stop the player if it is still running
-			clip.setFramePosition(0); // rewind to the beginning
-			clip.start();     // Start playing
-		}
+	if (clip.isRunning())
+		clip.stop();   // Stop the player if it is still running
+		clip.setFramePosition(0); // rewind to the beginning
+		clip.start();     // Start playing
+		
 	}
 
 	/* Optional static method to pre-load all the sound files 
